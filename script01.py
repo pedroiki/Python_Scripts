fdc1f93179d6578962b027f95ad39bdce6e5d9c9
Carlos Pedro Gomes <57367344+pedroiki@users.noreply.github.com>
2020-04-26 22:33:16 +0200

    basic print and variables
    basic print and variables

---
+++ script01.py
@@ -0,0 +1,32 @@
+print ("hello world")
+
+# numeros nao se usa aspas
+
+print (7+4)              # resultado 11
+print  ("7" + "4")       #resultado 74
+print ("ola" , 5 )   #resultado ola 5
+
+# variaveis , em python se escreve sempre letras minuscula com excepcao entre aspas,
+# toda a viagem e objecto em python
+#variaveis nome,idade,peso  todas variarem recebem valores e usam o sinal =
+#      =   quer dizer que a variavel recebe algo
+
+name= "Pedro"
+age= 25
+peso= 74.8
+
+print (nome, idade, peso)          #resultado Pedro 25 74.8
+print (nome + idade + peso)        #error
+
+# input quer dizer leia
+name= input ("qual o seu nome")
+age=  input ("quantos anos voce tem?")
+peso= input ("quando voce pesa?")
+
+nome = input ("qual o seu nome?")       #resultado qual o seu nome? exemplo : Carlos
+age= input ("quantos anos voce tem")    #resultado quantos anos voce tem? exemplo 30
+peso= input ("qual e o seu peso?")     #resultado qual o seu peso? exemplo 70
+
+print (nome, idade, peso)              #resultado Carlos 30 70
+
+# para executar :   run
